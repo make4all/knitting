ffd3a0eb548e3e3443533fd116613f04a17efd5e
Ty Wang <tywang@Tys-MacBook-Pro.local>
2022-07-28 21:56:57 -0700

    add constraint of hole locations

--- pattern_modification/modifications.py
+++ pattern_modification/modifications.py
@@ -43,6 +43,46 @@ def add_hole(knit_graph: Knit_Graph, hole_start_row:int, hole_start_wale:int, ho
         elif course_id % 2 == 1:
             loop_ids = course_to_loop_ids[course_id][pattern_width-(hole_start_wale+hole_width):pattern_width - hole_start_wale]
         node_to_delete.extend(loop_ids)
+    #check if any node violates the constraint for a node to be part of a hole
+    for node in node_to_delete:
+        #first, each node should have exactly one parent. 
+        #If more than one, it is part of a decrease; If less than one, it itself is an increase.
+        #Likewise, each node should have exactly one child.
+        #If no child, it is a slip.
+        parent_ids = [*knit_graph.graph.predecessors(node)]
+        child_ids = [*knit_graph.graph.successors(node)]
+        if len(parent_ids) != 1:
+            print(f'node {node} is not qualified for a hole for having {len(parent_ids)} parents')
+            exit()
+        if len(child_ids) != 1:
+            print(f'node {node} is not qualified for a hole for having {len(child_ids)} parents')
+            exit()
+        #Second, on knit graph, any edge related to the node must have a parent offset that is 0
+        parent_id = parent_ids[0]
+        parent_offset = knit_graph.graph[parent_id][node]["parent_offset"]
+        if parent_offset != 0:
+            print(f'node {node} is not qualified for a hole for having parent offset of {parent_offset}')
+            exit()
+        child_id = child_ids[0]
+        child_offset = knit_graph.graph[node][child_id]["parent_offset"]
+        if parent_offset != 0:
+            print(f'node {node} is not qualified for a hole for having child offset of {child_offset}')
+            exit()
+        #Third, on yarn graph, the number of yarn edges related to the node must be 2
+        #If less than one, it can be node on the border, or part of a short row
+        parent_ids = [*yarn.yarn_graph.predecessors(node)]
+        child_ids = [*yarn.yarn_graph.successors(node)]
+        if len(parent_ids) != 1 or len(child_ids) != 1:
+            print(f'node {node} is not qualified for a hole for having no parent node or child node on yarn')
+            exit()
+        #Fourth, on yarn graph, parent node and child node of the node should be on the same course
+        #If not, it can be a slip on sheet
+        parent_id = parent_ids[0]
+        child_id = child_ids[0]
+        if loop_ids_to_course[parent_id] != loop_ids_to_course[child_id]:
+            print(f'node {node} is not qualified for a hole for having parent node and child node that are not on the same course')
+            exit()
+    
     knit_graph.graph.remove_nodes_from(node_to_delete)
     yarn.yarn_graph.remove_nodes_from(node_to_delete)
     print('node_to_delete', node_to_delete)
@@ -105,7 +145,7 @@ def add_hole(knit_graph: Knit_Graph, hole_start_row:int, hole_start_wale:int, ho
     return knit_graph
 
 if __name__ == '__main__':
-    knit_graph = test_stst()
-    # knit_graph = test_cable()
-    knitGraph = add_hole(knit_graph, hole_start_row=1, hole_start_wale=2, hole_height=6, hole_width=2)
+    # knit_graph = test_stst()
+    knit_graph = test_cable()
+    knitGraph = add_hole(knit_graph, hole_start_row=1, hole_start_wale=5, hole_height=1, hole_width=1)
     visualize_knitGraph(knitGraph)
